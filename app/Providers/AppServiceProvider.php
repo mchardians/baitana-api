7e5465dbb3e64ab6bce7430d3989b56fed6256b1
rafioosadani <rafioosadani@gmail.com>
2025-07-06 08:38:07 +0700

    test deployment for baitana-api with nginx

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,10 +22,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        DB::listen(function($query) {
-            Log::channel("querylog")->debug("SQL: ". $query->sql);
-            Log::channel("querylog")->debug("Bindings: ". json_encode($query->bindings));
-            Log::channel("querylog")->debug("Time: ". $query->time. "ms");
-        });
+        //
     }
 }
